70fcacd632ba8c82a01f916ea321dbbcca486716
nAjss <42645550+nAjss@users.noreply.github.com>
2024-05-08 09:02:03 +0000

    number und string

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -1,18 +1,37 @@
-console.log("check");
+/* Deklaration + Wertzuweisung I
+
+let firstName; //deklaration
+
+firstName = "Max"; //wertzuweisung / assignment
+
+console.log(firstName); //ausgabe
+
+let familyName = "Connnnon";
+
+console.log("Hi, ich bin " + firstName + " " + familyName + "!");
+
+ */
 
 /*
+[10:36] Peter Schmidt
+  
 
-hallo was
+    Wertzuweisung II + Datentypen 
 
-tets
-
-sf
-
-f
-asdf
+    JS ist ein untypisierte Sprache!
 
 */
 
+let test;
+console.log("Inhalt: " + test);
+console.log("Datentyp: " + typeof test);
 
-/**** Variablen ****/
+test = "Hi";
+console.log("Datentyp: " + typeof test);
 
+test = 11,13;
+console.log("Datentyp: " + typeof test);
+
+test = 2;
+console.log("Inhalt: " + test);
+console.log("Datentyp: " + typeof test);
