d2d2659d36c720fa16155092c401ad951f03c612
nAjss <42645550+nAjss@users.noreply.github.com>
2024-05-13 08:52:02 +0000

    Datenübergabe von innen

--- src/08_funktionen_I.js
+++ src/08_funktionen_I.js
@@ -2,3 +2,26 @@
 /***** Funktionen 01 *****/
 // 1. Kapselung von Codeblöcken
 
+// Funktionsaufruf | call(er)
+// test();
+
+// Funktionsruf | Callee
+// Funktionsdeklaration
+function test() 
+{
+    console.log("Hallo tralala!");
+}
+
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
+
+// Funktionsaufruf
+
+outputNames();
+
+function outputNames(params) 
+{
+    let firstName = "Esther";
+
+    console.log("Hallo, " + firstName + "!");    
+}
