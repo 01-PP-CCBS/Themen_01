ca346e72005def383ae1ce8e0db33697f7984f12
nAjss <42645550+nAjss@users.noreply.github.com>
2024-05-14 09:47:36 +0000

    swutch Variante - div/0 - inhalt testen

--- src/09_funktionen_II.js
+++ src/09_funktionen_II.js
@@ -11,16 +11,41 @@
 */
 
 
+const prompt = require('prompt-sync')({sigint: true});
+const zahl1 = prompt("Bitte Zahl 1 eingeben: ");
+const zahl2 =  parseInt(prompt("Bitte Zahl 2 eingeben: ")); // "2" --> 2
+const grundRechenArt = prompt("Bitte +, -, *, / eingeben: ");
 
-
-
-
-
-
-// module: output | test:
-// output("hello");
-// output(2);
 function output(outputData) {
 	console.log(outputData);
+}
+
+if (!isNaN(zahl1 || zahl2)) 
+{
+	switch (grundRechenArt) {
+		case "+":
+			output(Number(zahl1) + Number(zahl2));	
+			break;
+		case "-":
+			output(Number(zahl1) - Number(zahl2));	
+			break;
+		case "*":
+			output(Number(zahl1) * Number(zahl2));	
+			break;
+		case "/":
+			if (zahl2 == 0) {	
+				output("div by 0");
+				break;			
+			}
+			output(Number(zahl1) / Number(zahl2));	
+			break;
+							
+		default:
+			output("Du hast leider keine richtige Eingabe getätigt!");	
+			break;
+	}
+}
+else{
+	output("keine Zahl eingegeben!")
 }
   
