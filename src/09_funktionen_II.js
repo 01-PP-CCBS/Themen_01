75a76b71faf4401ca3478fa4317baa4c6f9e9f8e
nAjss <42645550+nAjss@users.noreply.github.com>
2024-05-15 06:50:29 +0000

    peter funkritionen II und III ++

--- src/09_funktionen_II.js
+++ src/09_funktionen_II.js
@@ -14,15 +14,16 @@
 const prompt = require('prompt-sync')({sigint: true});
 const zahl1 = prompt("Bitte Zahl 1 eingeben: ");
 const zahl2 =  parseInt(prompt("Bitte Zahl 2 eingeben: ")); // "2" --> 2
-const grundRechenArt = prompt("Bitte +, -, *, / eingeben: ");
+const operator = prompt("Bitte +, -, *, / eingeben: ");
+const ERROR_STR_DIV = "div by zero";
 
 function output(outputData) {
 	console.log(outputData);
 }
 
-if (!isNaN(zahl1 || zahl2)) 
+if (!isNaN(zahl1) && !isNaN(zahl2)) 
 {
-	switch (grundRechenArt) {
+	switch (operator) {
 		case "+":
 			output(Number(zahl1) + Number(zahl2));	
 			break;
@@ -34,14 +35,14 @@ if (!isNaN(zahl1 || zahl2))
 			break;
 		case "/":
 			if (zahl2 == 0) {	
-				output("div by 0");
+				output(ERROR_STR_DIV);
 				break;			
 			}
 			output(Number(zahl1) / Number(zahl2));	
 			break;
 							
 		default:
-			output("Du hast leider keine richtige Eingabe getätigt!");	
+			output("Fehlerhafte Eingabe!");	
 			break;
 	}
 }
