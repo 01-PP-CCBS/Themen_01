22189ddd25e7d93d163c15f2b71729ba661e295c
nAjss <42645550+nAjss@users.noreply.github.com>
2024-05-15 08:25:02 +0000

    Arrays | 01 Fkt. mit Einzelparameter

--- src/11_arrays_I.js
+++ src/11_arrays_I.js
@@ -6,6 +6,13 @@
  * 
  */
 
+arr = [];
+arr = [2,11,7,8,10,21] // Array mit Elementen
+
+//output(arr.length); Anzahl Einträge
+output(arr[0]);  // erste Posi
+output(arr[arr.length-1]);  // letzte posi
+
 
 
 /**
@@ -18,15 +25,16 @@
 // --> "Ich bin Max Mütze."
 // output(getSentence("Ich","bin","Max","Mütze"));
 // output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
-function getSentence(word1,word2,word3,word4) {
+function getSentence(word1,word2,word3,word4,word5,word6) {
     const GAP = " ";
     const PUNCT = ".";
     let str =   word1 + GAP +
                 word2 + GAP +
                 word3 + GAP +
                 word4 + GAP +
+                word5 + GAP +
+                word6 +               
                 PUNCT;
-
     return str;
 }
 
