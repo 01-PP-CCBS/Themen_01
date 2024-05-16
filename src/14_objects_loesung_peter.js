62882595a8d127011def7b18ea4574dc7dac19f2
nAjss <42645550+nAjss@users.noreply.github.com>
2024-05-16 10:49:39 +0000

    nested objects

--- src/14_objects_loesung_peter.js
+++ src/14_objects_loesung_peter.js
@@ -1,6 +1,6 @@
 /***** Erinnerung: Arrays *******/
 
-let arr;
+// let arr;
 // arr = ["Ich","bin","Max"];
 // output(arr);
 // output(arr[0]);
@@ -30,16 +30,16 @@ let arr;
 
 /***** Objekte 1 Daten/Funktionen *******/
 
-let person = {
-                firstName:"Behrang",
-                familyName:"Valadkhani",
-                salary: [120000, 160000],
-                permission: true,
-                sayHello: function(){
-                    // return "Hallo, ich bin " + person.firstName;
-                    return "Hallo, ich bin " + this.firstName;
-                }
-            };
+// let person = {
+//                 firstName:"Jan",
+//                 familyName:"Conradt",
+//                 salary: [120000, 160000],
+//                 permission: true,
+//                 sayHello: function(){
+//                     // return "Hallo, ich bin " + person.firstName;
+//                     return "Hallo, ich bin " + this.firstName;
+//                 }
+//             };
 
 
 // output(person);
@@ -52,7 +52,7 @@ let person = {
 // const txt = person.sayHello() + ", ich bin " + person.firstName +
 //             " und verdiene " + person.salary[1] + " p.a."
 
-const txt = person.sayHello() + " und verdiene " + person.salary[1] + " p.a."
+// const txt = person.sayHello() + " und verdiene " + person.salary[1] + " p.a."
 
 // output(txt);
 
@@ -67,13 +67,13 @@ let baikal = {
                 value: "10m",
                 deep:{
                         deeper:{
-                            temperature:"4 Grad Ceslisus",
+                            temperature:"4 Grad Celsius",
                             deepest: "Das Licht - auf 1642m!"
                         }
                 }
             };
 
-output(baikal);
+// output(baikal);
 output(baikal.value);
 output(baikal.deep.deeper.temperature);
 output(baikal.deep.deeper.deepest);
